4d2b475df315df97ebb24cdd9352c77a8399e299
ryszardpanda <rychu48@op.pl>
2025-03-21 20:46:07 +0100

    +Added integration test foe gitHubClient

--- src/main/java/com/ryszardpanda/GitHubReposFetcher/client/GitHubRepoInfoClient.java
+++ src/main/java/com/ryszardpanda/GitHubReposFetcher/client/GitHubRepoInfoClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "apigithub", url = "https://api.github.com/", configuration = GitHubClientConfiguration.class)
+@FeignClient(value = "apigithub", url = "${base.url.apigithub}", configuration = GitHubClientConfiguration.class)
 public interface GitHubRepoInfoClient {
     @RequestMapping(method = RequestMethod.GET, value = "/repos/{owner}/{repo}")
     GitHubRepositoryDTO getRepoInfo(@PathVariable("owner") String owner, @PathVariable("repo") String repositoryName);
